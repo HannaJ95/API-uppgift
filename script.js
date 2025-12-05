1ef532141d44db4198fbeb4d1a3913a401b57207
Hanna Johansson <hannajohansson16@hotmail.com>
2025-12-05 10:39:41 +0100

    Updated JS with displaying drink + instructions

--- script.js
+++ script.js
@@ -1,34 +1,52 @@
 
+//---- URL ----//
 const base_url = 'https://www.thecocktaildb.com/api/json/v1/1';
 
+//---- DOM ----//
 const alcohol_select = document.getElementById('alcohol_select');
 const search_button = document.getElementById('search_button');
+// const drink_name_element = document.getElementById('drink-name');
+// const ingredients_list = document.getElementById('ingredients-list');
+
+
+//---- FUNKTIONER ----//
+function insertText(element_id, text){
+
+    const element = document.getElementById(element_id);
+
+    if (element.textContent === '') {
+        element.textContent = text;
+    } else {
+        element.textContent = text;
+    }
+}
+
 
 
 search_button.addEventListener('click', () => {
-    console.log('Vald alkohol:', alcohol_select.value);
     
-    const alcohol_type = alcohol_select.value;
     
     const get_drink = async () => {
-
+        
         try {
-
+            
             //1. Tar fram drinkar som innehåller vald alkohol
+            const alcohol_type = alcohol_select.value;
             const filter_url_part = '/filter.php?i=';
             const filter_url = base_url + filter_url_part + alcohol_type;
             
             const drinks_response = await fetch(filter_url);
-            const drinks = await drinks_response.json();
+            let drinks = await drinks_response.json();
+            drinks = drinks.drinks;
             
+            console.log('Vald alkohol:', alcohol_select.value);
             console.log("1. drinkar med vald alkohol: ", drinks);
 
             //2. Slumpar fram en av drinkarna
-            const random_index = Math.floor(Math.random() * drinks.drinks.length);
+            const random_index = Math.floor(Math.random() * drinks.length);
+            const drink_id = drinks[random_index]['idDrink'];
+            
             console.log("2. random_index: " + random_index);
-
-            const drink_id = drinks.drinks[random_index]['idDrink'];
-
             console.log("2. Drink id: " + drink_id);
 
 
@@ -37,11 +55,18 @@ search_button.addEventListener('click', () => {
             const lookup_url = base_url + lookup_url_part + drink_id;
 
             const details_response = await fetch(lookup_url);
-            const drink = await details_response.json();
+            let drink = await details_response.json();
+            drink = drink.drinks[0];
 
-            console.log("3. Drink details: " + drink.drinks[0]['strDrink']);
+            console.log("3. Drink details: " + drink['strDrink']);
+            console.log(drink);
 
-            const name = drink.drinks[0]['strDrink'];
+
+            const drink_name = drink['strDrink'];
+            const drink_instructions = drink['strInstructions'];
+
+            insertText('drink-name', drink_name);
+            insertText('instructions-text', drink_instructions);
             
 
         } catch (e) {
