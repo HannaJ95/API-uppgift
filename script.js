b82915fca794808639beffe6c2f59d01a3860e62
Hanna Johansson <hannajohansson16@hotmail.com>
2025-12-07 14:29:46 +0100

    Updated JS file with convert function to cl

--- script.js
+++ script.js
@@ -24,6 +24,35 @@ function get_ingredients_and_measure(drink) {
     return ingredients;
 }
 
+function convert_to_cl(measure) {
+    if (!measure) return '';
+    
+    measure = measure.trim().toLowerCase();
+    
+    const amount = parseFloat(measure);
+    
+    if (isNaN(amount)) return measure;
+    
+    if (measure.includes('ml')) {
+        return `${(amount / 10).toFixed(1)} cl`;
+    }
+    else if (measure.includes('oz')) {
+        return `${Math.round(amount * 3)} cl`;
+    } 
+    else if (measure.includes('cl')) {
+        return `${amount} cl`;
+    }
+    else if (measure.includes('cup')) {
+        return `${Math.round(amount * 24)} cl`;
+    }
+    else if (measure.includes('shot')) {
+        return `${amount * 4} cl`;
+    }
+    else {
+        return measure;
+    }
+}
+
 function build_ingredients_html(ingredients) {
     const ingredients_list = ingredients
         .map(ing => `<li>${ing.measure} ${ing.name}</li>`)
@@ -32,6 +61,18 @@ function build_ingredients_html(ingredients) {
     return ingredients_list;
 }
 
+function build_ingredients_cl_html(ingredients) {
+    const ingredients_list = ingredients
+        .map(ing => {
+            const converted_measure = convert_to_cl(ing.measure);
+            return `<li>${converted_measure} ${ing.name}</li>`;
+        })
+        .join('');
+
+    return ingredients_list;
+}
+
+//---- ASYNC FUNCTIONS ----//
 async function fetch_drinks_by_alcohol(alcohol) {
     const url = `${base_url}/filter.php?i=${alcohol}`;
     const response = await fetch(url);
@@ -82,7 +123,8 @@ search_button.addEventListener('click', async () => {
 
         //5. Get list with ingredients wrapped in html <li> tags
         const ingredients_and_measure = get_ingredients_and_measure(drink);
-        const ingredients_html_list = build_ingredients_html(ingredients_and_measure);
+        // const ingredients_html_list = build_ingredients_html(ingredients_and_measure);
+        const ingredients_html_list = build_ingredients_cl_html(ingredients_and_measure);
         
         console.log("Ingredienslista:", ingredients_and_measure);
 
